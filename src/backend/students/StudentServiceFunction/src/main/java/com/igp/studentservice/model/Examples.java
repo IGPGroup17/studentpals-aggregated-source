92ac7c0f5c2d11e8702d33637927eccac83289c9
OllieJonas <OllieJonas@users.noreply.github.com>
2021-02-03 14:31:51 +0000

    From student-service.

--- src/backend/students/StudentServiceFunction/src/main/java/com/igp/studentservice/model/Examples.java
+++ src/backend/students/StudentServiceFunction/src/main/java/com/igp/studentservice/model/Examples.java
@@ -1,11 +1,8 @@
 package com.igp.studentservice.model;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
 public class Examples {
 
-    public static final StudentBasic EXAMPLE_BASIC = new StudentBasic("johnbernadis93", "John Bernadis", 27, "Male");
+    public static final StudentBasic EXAMPLE_BASIC = new StudentBasic("johnbernadis93", "Leon Watts", 27, "Male");
 
     public static final StudentUni EXAMPLE_UNI = new StudentUni("johnbernadis93@gmail.com", "University of Bath", 2, "Computer Science (BSc) Hons");
 
