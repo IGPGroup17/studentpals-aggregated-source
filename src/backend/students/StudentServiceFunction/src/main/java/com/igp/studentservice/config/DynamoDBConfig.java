d5babff7d2df709ce2f8a8914ba7a7fa77e42a63
en410 <en410@users.noreply.github.com>
2021-02-16 10:20:58 +0000

    From student-service.

---
+++ src/backend/students/StudentServiceFunction/src/main/java/com/igp/studentservice/config/DynamoDBConfig.java
@@ -0,0 +1,20 @@
+package com.igp.studentservice.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.auth.EnvironmentVariableCredentialsProvider;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapperConfig;
+
+public class DynamoDBConfig {
+    public static DynamoDBMapper dynamoDBMapper() {
+        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+                .withCredentials(new EnvironmentVariableCredentialsProvider())
+                .withRegion(Regions.EU_WEST_2)
+                .build();
+        return new DynamoDBMapper(client, DynamoDBMapperConfig.DEFAULT);
+    }
+}
--- src/backend/students/StudentServiceFunction/src/main/java/com/igp/studentservice/model/Examples.java
+++ src/backend/students/StudentServiceFunction/src/main/java/com/igp/studentservice/model/Examples.java
@@ -6,6 +6,5 @@ public class Examples {
 
     public static final StudentUni EXAMPLE_UNI = new StudentUni("johnbernadis93@gmail.com", "University of Bath", 2, "Computer Science (BSc) Hons");
 
-
     public static final StudentDetailed EXAMPLE_DETAILED = new StudentDetailed(EXAMPLE_BASIC, EXAMPLE_UNI);
 }
